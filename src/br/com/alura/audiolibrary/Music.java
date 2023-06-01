e28564dfd2daac3739ea6f3347272490c1d30818
Brunna Serafina <brunnaserafina@gmail.com>
2023-06-01 00:31:24 -0300

    feat: implemented Music subclass with artist, genre, and album properties

---
+++ src/br/com/alura/audiolibrary/Music.java
@@ -0,0 +1,31 @@
+package br.com.alura.audiolibrary;
+
+public class Music extends Audio {
+    private String artist;
+    private String genre;
+    private String album;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
